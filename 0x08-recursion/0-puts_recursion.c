598c0cd5cea3789dc67f6dadea68352a119645f1
Ghada3333 <ghadahassan331992@gmail.com>
2023-04-05 05:07:41 -0700

    gh

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,20 @@
-/*                                                                                                                                      
- * File: 0-puts_recursion.c                                                                                                             
- * Auth: sam                                                                                                                            
- */                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * _puts_recursion - Prints a string, followed by a new line.                                                                           
- * @s: The string to be printed.                                                                                                        
- */                                                                                                                                     
-void _puts_recursion(char *s)                                                                                                           
-{                                                                                                                                       
-        if (*s)                                                                                                                         
-        {                                                                                                                               
-                _putchar(*s);                                                                                                           
-                _puts_recursion(s + 1);                                                                                                 
-        }                                                                                                                               
-                                                                                                                                        
-        else                                                                                                                            
-                _putchar('\n');                                                                                                         
+@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: On success 1.
+ * Base case ->  when s is 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
